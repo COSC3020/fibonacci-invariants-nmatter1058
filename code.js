44e596f7d743f38fd248276a60c0462f242e9c50
nmatter1058 <143532329+nmatter1058@users.noreply.github.com>
2023-11-21 21:50:10 -0700

    Fibonacci.js

--- code.js
+++ code.js
@@ -1,3 +1,17 @@
-function fib(n) {
-    return n;
+function fib(n){
+    var index = new Array(val+1);
+    var store = new Array(val+1);
+    for (i=0; i < index.length; i++){
+        index[i] = i;
+        store[i] = createFib(i);
+    }
+    console.log("Index: " + index);
+    console.log("Value: " + store + "\n");
+}
+
+function createFib(n){
+    if (n <= 1)
+        return n;
+    else 
+       return createFib(val-1) + createFib(val-2);    
 }
